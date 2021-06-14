3ee6a1ab4983656dbfeb8b966891ede29f3ead15
Alexsander Uglin <uglin76@mail.ru>
2021-06-15 00:48:30 +0300

    refactor: небольшой рефакторинг

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -6,16 +6,13 @@ from django.shortcuts import render, reverse
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
     
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -23,8 +20,6 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
     template_name = 'app/time.html'
     current_time = datetime.strftime(datetime.now(), "%H:%M:%S")
 
@@ -33,10 +28,8 @@ def time_view(request):
     }
     return render(request, template_name, context)
 
+
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
     template_name = 'app/files.html'
 
     context = {
