5080e4382e222c578edef6b56ee278ce7cee00e1
Alexsander Uglin <uglin76@mail.ru>
2021-06-17 12:52:30 +0300

    feat: дополнительное задание Обработка запросов и шаблонов решено

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+from csv import DictReader
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +10,16 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    with open('data-398-2018-08-30.csv', encoding='utf-8', newline='') as file:
+        reader = DictReader(file)
+        stations = [row for row in reader]
+
+        paginator = Paginator(stations, per_page=10)
+        current_page = request.GET.get('page', 1)
+        page = paginator.get_page(current_page)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+       'page': page,
+       'bus_stations': page.object_list,
     }
     return render(request, 'stations/index.html', context)
