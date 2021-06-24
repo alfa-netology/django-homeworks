a67cad31b14b677ef9d3008e8505e5022f5c9c2c
Alexsander Uglin <uglin76@mail.ru>
2021-06-24 18:08:38 +0300

    feat: refactor

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,7 +1,6 @@
-from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student, Teacher
+from .models import Student
 
 
 def students_list(request):
@@ -13,26 +12,5 @@ def students_list(request):
     for student in students:
         object_list.append({'student': student, 'teachers': student.teacher.all()})
 
-
-    print(students_list)
-
-
-    # for s in students:
-    #     if s['name'] not in students_counter:
-    #         students_counter.append(s['name'])
-    #         object_list.append({'name': s['name'], 'group': s['group'],
-    #                             'teachers': [{'name': s['teacher__name'], 'subject': s['teacher__subject']}]})
-    #     else:
-    #         for o in object_list:
-    #             if o['name'] == s['name']:
-    #                 o['teachers'].append({'name': s['teacher__name'], 'subject': s['teacher__subject']})
-    # context = {
-    #     'object_list': object_list
-    # }
-    # return render(request, template, context)
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
     context = {'object_list': object_list}
     return render(request, template, context)
