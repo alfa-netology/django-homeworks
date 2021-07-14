876d11f8100109caf889bcf580e09752946caa22
Alexsander Uglin <uglin76@mail.ru>
2021-07-14 13:18:42 +0300

    feat: tasks solved

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock, StockProduct
@@ -7,13 +8,18 @@ from logistic.serializers import ProductSerializer, StockSerializer, ProductPosi
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['products__title', 'products__description', 'products__id']
 
 
 class StockProductViewSet(ModelViewSet):
     queryset = StockProduct.objects.all()
     serializer_class = ProductPositionSerializer
+
